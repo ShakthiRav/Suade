261a5dfdaf3665184dd47ca6339025df87991e26
Shakthivel Ravichandran <caldays.r@gmail.com>
2020-06-22 21:41:21 +0100

    add flask stuff

---
+++ ReportGenerator.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from GetMetrics import *
+app = Flask(__name__)
+report = ""
+
+@app.route('/',  methods=['GET'])
+def hello():
+    return render_template('dashboard.html', report = report)
+
+@app.route('/',  methods=['POST'])
+def update():
+    date = request.form['date']
+    report = getReport(date)
+
+    return render_template('dashboard.html', report = report ,)
+
+    
+
+if __name__ == "__main__":
+    app.run(debug = True)
